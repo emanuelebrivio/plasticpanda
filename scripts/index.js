884ff5d98e3b4037ae9be71af28533d5ed68e201
Emanuele Brivio <emanuele@Monolith.lan>
2020-03-28 16:51:47 +0100

    Add isMobile

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,14 @@
   var mail = document.getElementById("email");
   var sayHi = document.querySelector(".say-hi");
 
-  mail.addEventListener("mouseenter", function() {
+  if (isMobile.phone) {
     sayHi.classList.add("hi");
-  });
-  mail.addEventListener("mouseleave", function() {
-    sayHi.classList.remove("hi");
-  });
+  } else {
+    mail.addEventListener("mouseenter", function() {
+      sayHi.classList.add("hi");
+    });
+    mail.addEventListener("mouseleave", function() {
+      sayHi.classList.remove("hi");
+    });
+  }
 })();
