d0e3465cf84379a3aa62f2d95e257351b1e453c5
Emanuele Brivio <emanuele@Monolith.lan>
2020-03-28 15:48:52 +0100

    Add script

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+(function() {
+  var mail = document.getElementById("email");
+  var logo = document.getElementById("logo");
+
+  mail.addEventListener("mouseenter", function() {
+    console.log("Enter");
+  });
+})();
